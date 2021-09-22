66cc594b50075cc7f192302892f9d2cb033d0d86
Huangqz <flaggyellow@qq.com>
2021-09-22 04:42:05 +0000

    starting test

---
+++ build/src/CMakeFiles/cuckoo.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cuckoo.dir/bucket.cc.o"
+  "CMakeFiles/cuckoo.dir/cuckoo.cc.o"
+  "CMakeFiles/cuckoo.dir/md5.cc.o"
+  "CMakeFiles/cuckoo.dir/murmur3.c.o"
+  "libcuckoo.pdb"
+  "libcuckoo.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/cuckoo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/test/CMakeFiles/perform.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/perform.dir/performTest.cc.o"
+  "perform"
+  "perform.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/perform.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
